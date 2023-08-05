ea64e8be3088795a832d7066efe581186cc7e499
Meezan Shaikh <meezan@tradexa.co.in>
2023-08-05 14:49:04 +0530

    get shoping list added with firebase

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,13 +1,18 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {RecipeService} from '../recipes/recipe.service';
-import {Recipe} from '../recipes/recipe.model';
-import {map, tap} from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
+import { map, tap } from 'rxjs/operators';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Ingredient } from './ingredient.model';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class DataStorageService {
 
-  constructor(private http: HttpClient, private recipeService: RecipeService) {
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecipeService,
+    private shoppingListService: ShoppingListService) {
   }
 
   createRecipes() {
@@ -17,6 +22,18 @@ export class DataStorageService {
       .subscribe(res => {
         console.log(res);
       });
+    const shoppingList = this.shoppingListService.getIngredients();
+
+    this.http.put(
+      'https://recipe-angular-98507-default-rtdb.firebaseio.com/ingredient.json', shoppingList)
+      .subscribe(res => {
+        console.log(res);
+      });
+  }
+
+  retrieveIngredients() {
+    return this.http.get<Ingredient[]>(
+      'https://recipe-angular-98507-default-rtdb.firebaseio.com/ingredient.json')
   }
 
   retrieveRecipes() {
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,7 +1,9 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {Ingredient} from '../shared/ingredient.model';
-import {ShoppingListService} from './shopping-list.service';
-import {Subscription} from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
+import { of, Subscription } from 'rxjs';
+import { DataStorageService } from '../shared/data-storage.service';
+import { catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-shopping-list',
@@ -13,13 +15,23 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
   private ingredientsChangedSubscription: Subscription;
 
-  constructor(private shoppingListService: ShoppingListService) {
+  constructor(
+    private shoppingListService: ShoppingListService,
+    private dataStorageService: DataStorageService) {
   }
 
   ngOnInit() {
     this.ingredients = this.shoppingListService.getIngredients();
     this.ingredientsChangedSubscription = this.shoppingListService.ingredientsChanged.subscribe((ingredients: Ingredient[]) => {
       this.ingredients = ingredients;
+    });
+
+    this.dataStorageService.retrieveIngredients().pipe(
+      catchError(() => {
+        return of([])
+      })
+    ).subscribe((data) => {
+      this.shoppingListService.addIngredients(data);
     });
   }
 
